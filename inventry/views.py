0ed1413ae4b542481f7fb16ee213abe1c48e0210
Mazharaliruk <mazharjameel1020@gmail.com>
2024-11-05 08:46:16 -0800

    ok

--- inventry/views.py
+++ inventry/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category
+from .serializer import CategorySerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many = True)
+    return Response(serializer.data)
+    
