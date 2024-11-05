359c6e0b8e6919dd69bbbce8b9db8bac1b11d064
Mazharaliruk <mazharjameel1020@gmail.com>
2024-11-05 11:36:04 -0800

    ok

--- sales/serializer.py
+++ sales/serializer.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from .models import  Order
+from .models import  Order, Transaction, Payment
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
+        fields = '__all__'
+        
+        
+        
+# Transaction Serializer
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        
+# Payment Serializer
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
         
