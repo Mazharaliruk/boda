30f106bcaa8bcb9f0f6e9147ff75ad00ae4e294d
Mazharaliruk <mazharjameel1020@gmail.com>
2024-11-06 08:33:24 -0800

    ok

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import orders
 
-
+#GET /api/order_list/?status=COMPLETED&currency=INR&min_amount=100&max_amount=200&min_discount=10&max_discount=20
+# 
 # URLS
 urlpatterns = [
     path('orders/', orders, name='orders')
