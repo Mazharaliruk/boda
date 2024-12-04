f82ed5d9c727f3fad9d9c0ddb49a70f069f8c1f9
Mazharaliruk <97372288+Mazharaliruk@users.noreply.github.com>
2024-12-04 11:15:37 +0500

    auth added into inventry

--- inventry/views.py
+++ inventry/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from inventry.models import Category, SubCategory, Promotion, Discount, Tax
 from inventry.serializer import CategorySerializer, SubCategorySerializer, PromotionSerializer, DiscountSerializer, TaxSerializer
@@ -7,23 +8,28 @@ from inventry.serializer import CategorySerializer, SubCategorySerializer, Promo
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
 
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
     
     
 class PromotionViewSet(viewsets.ModelViewSet):
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
     
     
 class DiscountViewSet(viewsets.ModelViewSet):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
     
     
 class TaxViewSet(viewsets.ModelViewSet):
     queryset = Tax.objects.all()
     serializer_class = TaxSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
